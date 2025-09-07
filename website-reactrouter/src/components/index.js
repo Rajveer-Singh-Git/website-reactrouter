f609079edc657f7ad2b59fb016d47a7e5e7d3c03
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2025-09-07 15:08:58 +0530

    created custom components

---
+++ website-reactrouter/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import Footer from "./Footer/Footer";
+import About from "./About/About";
+import Contact from "./Contact/Contact"
+
+export {Header}
+export {Footer}
+export {Home}
+export {About}
+export {Contact}
